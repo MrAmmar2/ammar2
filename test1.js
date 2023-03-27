6fcc357aa619fc0caeb849d8583167768252bb4b
Ammar <ammarpauzan@gmail.com>
2023-03-27 15:39:37 +0800

    exercise 2

---
+++ test1.js
@@ -0,0 +1,26 @@
+let data1 = 1
+let data2 = "1"
+console.log(data1,typeof data1)
+console.log(data2,typeof data2)
+
+let data3 = data1 + data2
+console.log(data3,typeof data3)
+
+//Array
+
+let data = ["one", 2 , true]
+console.log(data[2],typeof data[2])
+
+//object 
+let user = {
+    name: "soo",
+    faculty: "fkekk",
+    phone:{
+        mobile:"0112034911",
+        office:"032134462"
+    },
+    email:"sdandsb@gmail.com"
+}
+console.log(user.faculty)
+console.log(user.email)
+console.log(user.phone.office)
